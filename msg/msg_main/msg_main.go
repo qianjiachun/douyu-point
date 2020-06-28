d69613514cf585b59160016f61d616827965153d
qianjiachun <189964430@qq.com>
2020-06-28 23:52:32 +0800

    init

--- msg/msg_main/msg_main.go
+++ msg/msg_main/msg_main.go
@@ -25,7 +25,7 @@ func handleFields(data string, fields []global.Field) {
 	for i := 0; i < len(fields); i++ {
 		var fieldCnt string
 		var fieldValue string // 字段值
-		var cnt int // 相乘值
+		var cnt int           // 相乘值
 
 		fieldValue = common.GetFieldValue(data, fields[i].Name)
 		fieldCnt = fields[i].Cnt
@@ -40,7 +40,7 @@ func handleFields(data string, fields []global.Field) {
 	}
 }
 
-func handleRules(data string, fieldValue string, cnt int , fieldRules []global.Rule, fieldDeafult global.Default) {
+func handleRules(data string, fieldValue string, cnt int, fieldRules []global.Rule, fieldDeafult global.Default) {
 	matchedNum := 0 // 满足条件的个数。当所有rule都不满足，则执行default的rule
 	for i := 0; i < len(fieldRules); i++ {
 		// 先判断有没有开播
@@ -70,32 +70,31 @@ func handleRules(data string, fieldValue string, cnt int , fieldRules []global.R
 		if isMatch {
 			matchedNum++
 			// 符合rule，开始执行积分操作
-			var tempChange string // 变化值
+			var tempChange string                          // 变化值
 			tempType := common.GetFieldValue(data, "type") // type名
-			tempUid := common.GetFieldValue(data, "uid") // 用户uid
-			tempId := common.GetFieldValue(data, "nn") // 用户id
+			tempUid := common.GetFieldValue(data, "uid")   // 用户uid
+			tempId := common.GetFieldValue(data, "nn")     // 用户id
 			if item.Limit > 0 {
 				// 需要限制
 				// 格式：list[ruleName][uid] = num
-				limitNum := global.List[tempType + "_" + item.Value][tempUid] // 已变化的次数
+				limitNum := global.List[tempType+"_"+item.Value][tempUid] // 已变化的次数
 				if limitNum < item.Limit {
 					// 满足限制条件
 					var newCnt int
-					if limitNum + cnt <= item.Limit {
+					if limitNum+cnt <= item.Limit {
 						newCnt = cnt
 					} else {
 						newCnt = item.Limit - limitNum
 					}
-					if _, ok := global.List[tempType + "_" + item.Value]; ok {
-						global.List[tempType + "_" + item.Value][tempUid] = limitNum + newCnt
+					if _, ok := global.List[tempType+"_"+item.Value]; ok {
+						global.List[tempType+"_"+item.Value][tempUid] = limitNum + newCnt
 					} else {
 						temp := make(map[string]int)
 						temp[tempUid] = limitNum + newCnt
-						global.List[tempType + "_" + item.Value] = temp
+						global.List[tempType+"_"+item.Value] = temp
 					}
 					tempChange = strconv.Itoa(newCnt * item.Change)
 					// 插入到数据库
-					println("哇:"+tempChange)
 					db.InsertData(tempUid, tempId, tempChange)
 				}
 			} else {
@@ -120,29 +119,29 @@ func handleRules(data string, fieldValue string, cnt int , fieldRules []global.R
 			return
 		}
 
-		var tempChange string // 变化值
+		var tempChange string                          // 变化值
 		tempType := common.GetFieldValue(data, "type") // type名
-		tempUid := common.GetFieldValue(data, "uid") // 用户uid
-		tempId := common.GetFieldValue(data, "nn") // 用户id
+		tempUid := common.GetFieldValue(data, "uid")   // 用户uid
+		tempId := common.GetFieldValue(data, "nn")     // 用户id
 
 		if fieldDeafult.Limit > 0 {
-			limitNum := global.List[tempType + "_" + "default"][tempUid]
+			limitNum := global.List[tempType+"_"+"default"][tempUid]
 			if limitNum < fieldDeafult.Limit {
 				// 满足限制条件
 
 				var newCnt int
-				if limitNum + cnt <= fieldDeafult.Limit {
+				if limitNum+cnt <= fieldDeafult.Limit {
 					newCnt = cnt
 				} else {
 					newCnt = fieldDeafult.Limit - limitNum
 				}
 
-				if _, ok := global.List[tempType + "_" + "default"]; ok {
-					global.List[tempType + "_" + "default"][tempUid] = limitNum + newCnt
+				if _, ok := global.List[tempType+"_"+"default"]; ok {
+					global.List[tempType+"_"+"default"][tempUid] = limitNum + newCnt
 				} else {
 					temp := make(map[string]int)
 					temp[tempUid] = limitNum + newCnt
-					global.List[tempType + "_" + "default"] = temp
+					global.List[tempType+"_"+"default"] = temp
 				}
 
 				tempChange = strconv.Itoa(newCnt * fieldDeafult.Change)
